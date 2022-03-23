fd538605534346f8c2f6113eaafc3e430d259e26
Johannes Dillmann <j.dillmann@sap.com>
2022-03-23 10:12:32 +0100

    Fake stack id io.buildpacks.stacks.bionic due to go dep
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- create-stack/test/acceptance/init_test.go
+++ create-stack/test/acceptance/init_test.go
@@ -57,7 +57,7 @@ const (
 	DistroName    = "Ubuntu"
 	DistroVersion = "18.04"
 	Homepage      = "https://github.com/paketo-buildpacks/stacks"
-	FocalStackID  = "io.buildpacks.stacks.focal"
+	FocalStackID  = "io.buildpacks.stacks.bionic"
 	TinyStackID   = "io.paketo.stacks.tiny"
 	Maintainer    = "Paketo Buildpacks"
 )
